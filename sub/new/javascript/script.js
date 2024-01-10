b156f765f030384205c182cf6d1824b8b76b0447
leehyean <93965591+leehyean@users.noreply.github.com>
2024-01-11 00:00:20 +0900

    서브 페이지(new) 추가
    1. 서브 페이지(new)를 추가함
    2. 메인 페이지의 섹션3,4,5를 모바일 사이즈에서 보이는 타이틀 크기를 줄임

---
+++ sub/new/javascript/script.js
@@ -0,0 +1,48 @@
+
+AOS.init();
+
+// var swiper = new Swiper(".section_1 .mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   loop: true,
+//   autoplay: true,
+//   autoplay: {
+//     delay: 4000,
+
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   breakpoints: { 반응형 width 
+
+//     1020: { 640 이상일 경우
+
+//     slidesPerView: 1,  }, 레이아웃 2 },
+
+//     768: { 
+//       slidesPerView: 3, 
+//     },
+
+//     },
+
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
